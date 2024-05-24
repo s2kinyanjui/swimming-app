7c08e5e23ee32c0b644556aad82538942f52d072
s2kinyanjui <s2kinyanjui@gmail.com>
2024-05-24 20:29:59 +0300

    convert to pwa

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,24 @@ import "@mantine/carousel/styles.css";
 import { ModalsProvider } from "@mantine/modals";
 
 import { createTheme, MantineProvider } from "@mantine/core";
+import { useEffect } from "react";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
 });
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((reg) => console.log("Service Worker registered"))
+        .catch((err) =>
+          console.error("Service Worker registration failed", err)
+        );
+    }
+  }, []);
+
   return (
     <MantineProvider theme={theme}>
       <ModalsProvider>
